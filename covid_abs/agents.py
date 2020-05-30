0e4a66e1fe9eba5f7d51fdf247233212fef92eed
hazemahmad <hazemabdelhaseeb@gmail.com>
2020-05-30 12:07:40 +0200

    try to resolve the attr error 1

--- covid_abs/agents.py
+++ covid_abs/agents.py
@@ -74,7 +74,7 @@ class Agent(object):
         """
         if self.infected_time <= self.incubation_time and self.status == Status.Infected:
             self.status = Status.Exposed
-            return "{}".format(self.status.name)
+            return "{}({})".format(self.status.name)
         elif self.status == Status.Infected and self.infected_time > 4:
             return "{}({})".format(self.status.name, self.infected_status.name)
         else:
