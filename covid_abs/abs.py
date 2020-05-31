e251ba262eccc5e3afe7808186a9a141967e9978
hazemahmad <hazemabdelhaseeb@gmail.com>
2020-05-31 02:01:33 +0200

    With Status Exposed

--- covid_abs/abs.py
+++ covid_abs/abs.py
@@ -209,7 +209,7 @@ class Simulation(object):
         if agent.status == Status.Exposed: 
             agent.infected_time += 1
             agent.infected_status = InfectionSeverity.Exposed
-            if agent.infected_time > agent.incubation_time :
+            if agent.infected_time > 6 :
                 agent.status = Status.Infected
                 
         if agent.status == Status.Death:
